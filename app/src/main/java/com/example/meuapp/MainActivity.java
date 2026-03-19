eb5379441373b63cd1afc53d6ea1e1a2c0ac36dc
Your Name <you@example.com>
2026-03-19 12:01:01 -0300

    sorteio com intervalo pronto

--- app/src/main/java/com/example/meuapp/MainActivity.java
+++ app/src/main/java/com/example/meuapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.meuapp;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -13,7 +14,8 @@ public class MainActivity extends AppCompatActivity {
 
     Button b;
     TextView tv;
-    int contador = 0;
+    EditText edmin,edmax;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,10 +24,28 @@ public class MainActivity extends AppCompatActivity {
         getString(R.string.app_name);
         b = findViewById(R.id.button);
         tv = findViewById(R.id.textView);
+        edmin = findViewById(R.id.edMin);
+        edmax = findViewById(R.id.edMax);
+
         b.setOnClickListener(v -> {
-            contador ++;
+            String minStr= edmin.getText().toString();
+            String maxStr= edmax.getText().toString();
+            if (minStr.isEmpty()){
+                edmin.setError("Informe o valor mínimo.");
+                edmin.requestFocus();
+                return;
+            }
+            if (maxStr.isEmpty()){
+                edmax.setError("Informe o valor Máximo.");
+                edmax.requestFocus();
+                return;
+            }
+
+            int min= Integer.parseInt(minStr);
+            int max= Integer.parseInt(maxStr);
+
             Random random = new Random();
-            int r = random.nextInt(100);
+            int r = random.nextInt(min, max);
             tv.setText(Integer.toString(r));
         });
     }

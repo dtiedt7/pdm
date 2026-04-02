911bf4a6813b2cf2a723829659061ae9e1a4b82b
yasmincvieira <vieiracyasmin>
2026-04-02 11:39:04 -0300

    fotinhas

--- app/src/main/java/com/example/meuapp/MainActivity.java
+++ app/src/main/java/com/example/meuapp/MainActivity.java
@@ -33,9 +33,23 @@ public class MainActivity extends AppCompatActivity {
         bntVoltar=findViewById(R.id.buttonVoltar);
         bntAvancar=findViewById(R.id.buttonAvancar);
         imageView=findViewById(R.id.imageView);
+        imageView.setImageResource(imagens[posicao]);
+
+        bntVoltar.setOnClickListener(v -> {
+            posicao--;
+            if (posicao<0){
+                posicao= imagens.length-1;
+            }
+            imageView.setImageResource(imagens[posicao]);
+        });
+
 
         bntAvancar.setOnClickListener(v ->{
-
+            if (posicao==imagens.length-1){
+                posicao=0;
+            }else{
+                posicao++;
+            }
 
             imageView.setImageResource(imagens[posicao]);
 

c777eaf649e16a654678e83676688f589a520d61
Your Name <you@example.com>
2026-04-02 12:01:47 -0300

    commit aula

--- app/src/main/java/com/example/meuapp/MainActivity.java
+++ app/src/main/java/com/example/meuapp/MainActivity.java
@@ -13,16 +13,6 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnAvancar, btnVoltar;
-    ImageView imageView;
-    Integer imagens[] = new Integer[]{
-            R.drawable.cachorro,
-            R.drawable.gardem,
-            R.drawable.happy,
-            R.drawable.patinho,
-            R.drawable.porquinho
-    };
-    int posicao = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,27 +20,6 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        btnAvancar=findViewById(R.id.buttonAvancar);
-        btnVoltar=findViewById(R.id.buttonVoltar);
-        imageView=findViewById(R.id.imageView);
-        imageView.setImageResource(imagens[posicao]);
-
-        btnAvancar.setOnClickListener(v -> {
-            if (posicao == imagens.length-1){
-                posicao =-1;
-            }
-            posicao++;
-            imageView.setImageResource(imagens[posicao]);
-
-        });
-
-        btnVoltar.setOnClickListener(v -> {
-            if (posicao <= 0){
-                posicao = imagens.length;
-            }
-            posicao--;
-            imageView.setImageResource(imagens[posicao]);
-        });
 
     }
 }

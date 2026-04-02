ff14c7ab8bb1228b2e88cdfa7ab450e10a8e9670
yasmincvieira <vieiracyasmin>
2026-04-02 11:52:17 -0300

    imc

--- app/src/main/java/com/example/meuapp/MainActivity.java
+++ app/src/main/java/com/example/meuapp/MainActivity.java
@@ -12,16 +12,7 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button bntAvancar, bntVoltar;
-    ImageView imageView;
 
-    Integer imagens[] = new Integer[]{
-            R.drawable.cachorro,
-            R.drawable.gardem,
-            R.drawable.happy,
-            R.drawable.patinho,
-            R.drawable.porquinho
-    };
 
     int posicao=0;
 
@@ -30,30 +21,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bntVoltar=findViewById(R.id.buttonVoltar);
-        bntAvancar=findViewById(R.id.buttonAvancar);
-        imageView=findViewById(R.id.imageView);
-        imageView.setImageResource(imagens[posicao]);
 
-        bntVoltar.setOnClickListener(v -> {
-            posicao--;
-            if (posicao<0){
-                posicao= imagens.length-1;
-            }
-            imageView.setImageResource(imagens[posicao]);
-        });
-
-
-        bntAvancar.setOnClickListener(v ->{
-            if (posicao==imagens.length-1){
-                posicao=0;
-            }else{
-                posicao++;
-            }
-
-            imageView.setImageResource(imagens[posicao]);
-
-        } );
 
     }
 }

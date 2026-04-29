dd146a992177c5d622e25b9c9969185eb80e259b
dtiedt7 <daniel.t2008@aluno.ifsc.edu.br>
2026-04-28 21:54:27 -0300

    commitzao da galera

--- app/src/main/java/com/example/meuapp/ActivityB.java
+++ app/src/main/java/com/example/meuapp/ActivityB.java
@@ -7,9 +7,6 @@ import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class ActivityB extends AppCompatActivity {
 
@@ -17,22 +14,53 @@ public class ActivityB extends AppCompatActivity {
     TextView tvPeso, tvAltura, tvIMC;
     Button btnFinish;
 
+    String[] classificacao = {
+            "Abaixo do peso",
+            "Normal",
+            "Sobrepeso",
+            "Obesidade",
+            "Obesidade Grave"
+    };
+
+    int[] imagens = {
+            R.drawable.abaixopeso,
+            R.drawable.normal,
+            R.drawable.sobrepeso,
+            R.drawable.obesidade1,
+            R.drawable.obesidade3
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_b);
-        iv= findViewById(R.id.imageView);
-        tvPeso= findViewById(R.id.tvPeso);
-        tvAltura= findViewById(R.id.tvAltura);
-        tvIMC= findViewById(R.id.tvIMC);
-        double altura = getIntent().getExtras().getDouble("altura");
-        double peso = getIntent().getExtras().getDouble("peso");
-        tvPeso.setText(peso);
-        tvAltura.setText(altura);
-        double imc = peso / (altura * altura);
-        Double.toString(imc);
-        tvIMC.setText(imc);
 
+        iv = findViewById(R.id.imageView);
+        tvPeso = findViewById(R.id.tvPeso);
+        tvAltura = findViewById(R.id.tvAltura);
+        tvIMC = findViewById(R.id.tvIMC);
+
+        double altura = getIntent().getDoubleExtra("altura", 0);
+        double peso = getIntent().getDoubleExtra("peso", 0);
+
+        tvPeso.setText(String.valueOf(peso));
+        tvAltura.setText(String.valueOf(altura));
+
+        double imc = peso / (altura * altura);
+
+        int indice = getIndiceIMC(imc);
+
+        tvIMC.setText(String.format("%.2f - %s", imc, classificacao[indice]));
+
+        iv.setImageResource(imagens[indice]);
+    }
+
+    private int getIndiceIMC(double imc) {
+        if (imc < 18.5) return 0;
+        else if (imc < 25) return 1;
+        else if (imc < 30) return 2;
+        else if (imc < 40) return 3;
+        else return 4;
     }
 }
